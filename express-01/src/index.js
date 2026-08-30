ce34749b8f05e8a4379d65d622e358e501ea12db
gabriellnobrega7 <gabriellnobrega21@gmail.com>
2026-08-30 13:17:30 -0300

    foi

--- express-01/src/index.js
+++ express-01/src/index.js
@@ -1,1 +1,27 @@
-console.log("Node.js funcionando!");
+import 'dotenv/config';
+import cors from 'cors';
+import express from 'express';
+
+const app = express();
+
+app.use(cors());
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.get('/hello', (req, res) => {
+  res.send('Olá, Turma!');
+});
+
+const port = process.env.PORT || 3000;
+
+// Executa o servidor apenas quando rodar localmente (não no ambiente Vercel)
+if (process.env.NODE_ENV !== 'production') {
+  app.listen(port, () =>
+    console.log(`Example app listening on port ${port}!`),
+  );
+}
+
+// Exportação necessária para a Vercel tratar o Express como Serverless Function
+module.exports = app;
